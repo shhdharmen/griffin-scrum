00a1ade4a8a1f762b265f5b336b8ff9b28b64238
Dharmen Shah <dharmen@quarkstudios.com>
2019-11-21 18:32:57 +0530

    added font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
-import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
+// import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+
+import { LayoutModule } from '@angular/cdk/layout';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
 import { GeneratorWrapperComponent } from './generator-wrapper/generator-wrapper.component';
 import { PreviewWrapperComponent } from './preview-wrapper/preview-wrapper.component';
 
@@ -30,8 +34,9 @@ import { PreviewWrapperComponent } from './preview-wrapper/preview-wrapper.compo
     MatToolbarModule,
     LayoutModule,
     MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    // MatIconModule,
+    MatListModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-wrapper/main-wrapper.component.ts
+++ src/app/main-wrapper/main-wrapper.component.ts
@@ -3,19 +3,24 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
+import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-main-wrapper',
   templateUrl: './main-wrapper.component.html',
   styleUrls: ['./main-wrapper.component.scss']
 })
 export class MainWrapperComponent {
-
+  faCoffee = faCoffee;
+  faBars = faBars;
+  faGithub = faGithub;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
 }
